4c9cd8f79a1a2ba5c040401ecd5da2b7a786f637
Bolaji Olajide <bolaji.olajide@andela.com>
2018-08-15 22:08:41 +0100

    create friendlist app with mobx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { inject, observer } from 'mobx-react';
+
 import './App.css';
 
+
+@inject('FriendStore')
+@observer
 class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    const friend = this.friend.value;
+    if (friend) {
+      this.props.FriendStore.addFriend(friend);
+    }
+    this.friend.value = '';
+  }
+
   render() {
+    const { FriendStore } = this.props;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>You have {FriendStore.friendCount} friends.</h2>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input type="text" placeholder="Enter friend" ref={input => this.friend = input}/>
+          <button>Add Friend</button>
+        </form>
+        <ul>
+          {FriendStore.friends.map(friend => (
+            <li key={friend}>{friend}</li>
+          ))}
+        </ul>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+
 import './index.css';
 import App from './App';
+import FriendStore from './stores/FriendStore';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Main = () => (
+  <Provider FriendStore={FriendStore}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
